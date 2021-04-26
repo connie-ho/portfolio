53e05da84777ddb3a4b6c9a1ca0e6c89b700ed91
Connie Ho <hconnie09@gmail.com>
2021-04-26 13:13:17 -0600

    change about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -132,7 +132,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'React', 'Eleventy', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['React', 'Python', 'Javascript', 'PostgreSQL', 'Flask', 'Ruby'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -142,21 +142,16 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Connie and I enjoy creating things that live on the internet. My
+              interest in software development started from winning United Way's
+              &quot;Disrupt-athon&quot; where I pitched an idea to combat Canada's food waste issue.
+              It was so much fun, but I had a nagging feeling that something was missing - I wanted
+              to develop the idea as a product myself!
             </p>
 
             <p>
-              Fast-forward to today, and I've had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, I can proudly say I am a full-stack developer. I love all
+              things tech and can't wait to keep growing my skills.
             </p>
 
             <p>Here are a few technologies I've been working with recently:</p>
--- src/config.js
+++ src/config.js
@@ -1,26 +1,14 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'connie3@ualberta.ca',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
-    },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      url: 'https://github.com/connie-ho',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
-    },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
+      url: 'https://www.linkedin.com/in/connie-h0',
     },
   ],
 
@@ -30,11 +18,7 @@ module.exports = {
       url: '/#about',
     },
     {
-      name: 'Experience',
-      url: '/#jobs',
-    },
-    {
-      name: 'Work',
+      name: 'Projects',
       url: '/#projects',
     },
     {
